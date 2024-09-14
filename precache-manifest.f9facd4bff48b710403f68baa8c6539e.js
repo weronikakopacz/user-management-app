268f27cdb3cce4c3bc1465ad5a422ad72389c2be
s481835 <werkop9@st.amu.edu.pl>
2024-09-14 14:30:35 +0200

    Updates

---
+++ precache-manifest.f9facd4bff48b710403f68baa8c6539e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b41fe3c876e0e7a2f73b9359a20529",
+    "url": "/user-management-app/index.html"
+  },
+  {
+    "revision": "39d6b6fc0af54f6176b7",
+    "url": "/user-management-app/static/css/main.28bdb016.chunk.css"
+  },
+  {
+    "revision": "adcd988b88095503e02e",
+    "url": "/user-management-app/static/js/2.ae910bde.chunk.js"
+  },
+  {
+    "revision": "39d6b6fc0af54f6176b7",
+    "url": "/user-management-app/static/js/main.a8175d13.chunk.js"
+  },
+  {
+    "revision": "e5a7a0b1034674f5b583",
+    "url": "/user-management-app/static/js/runtime~main.06a3e3e8.js"
+  }
+]);
